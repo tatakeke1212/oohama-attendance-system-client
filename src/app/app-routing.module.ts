e7d44d252386848f221efd28dbd824edd64a2b27
tatakeke1212 <take.soccer.1999@outlook.jp>
2022-11-13 17:12:31 +0900

    OASC-0004 トップ画面と従業員選択画面のファイル追加＆ディレクトリ整理

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { TopComponent } from './top/top.component';
+import { AttendanceComponent } from './pages/attendance/attendance.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SelectEmployeeComponent } from './pages/select-employee/select-employee.component';
+import { TopComponent } from './pages/top/top.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/top', pathMatch: 'full' },
   { path: 'top', component: TopComponent },
+  { path: 'selectEmployee', component: SelectEmployeeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'attendance', component: AttendanceComponent },
 ];
 
 @NgModule({
