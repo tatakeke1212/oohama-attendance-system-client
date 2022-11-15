0e8c8681f5356f87aea0f5f066f874e1a3c57a22
tatakeke1212 <take.soccer.1999@outlook.jp>
2022-11-16 02:07:35 +0900

    OASC-0005 従業員選択画面で、APIから取得した従業員一覧をもとにループでボタンを表示させる

--- src/app/pages/select-employee/select-employee.component.ts
+++ src/app/pages/select-employee/select-employee.component.ts
@@ -5,29 +5,24 @@ import { base_url } from 'src/environments/environment';
 @Component({
   selector: 'app-select-employee',
   templateUrl: './select-employee.component.html',
-  styleUrls: ['./select-employee.component.css']
+  styleUrls: ['./select-employee.component.css'],
 })
 export class SelectEmployeeComponent implements OnInit {
   /** 従業員一覧 */
-  employeeList:any = [];
+  employeeList: any = [];
 
-  user = '';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     // 従業員一覧を取得
-    this.getAllEmployees()
-    .subscribe((res) => {
+    this.getAllEmployees().subscribe((res) => {
       console.log(res);
       this.employeeList = res;
-      this.user = this.employeeList[0].name;
     });
   }
-  
+
   /** 従業員一覧を取得 */
   getAllEmployees() {
     return this.http.get<any>(base_url + 'api/employee/');
   }
-
 }
