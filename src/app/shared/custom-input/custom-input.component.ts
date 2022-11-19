d94a59e3ea0990eaff0c3afd6bd6674f15cbc1b6
tatakeke1212 <take.soccer.1999@outlook.jp>
2022-11-19 17:47:16 +0900

    OASC-0006 ログイン処理追加（APIとの通信）

--- src/app/shared/custom-input/custom-input.component.ts
+++ src/app/shared/custom-input/custom-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-custom-input',
@@ -6,9 +6,21 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./custom-input.component.css'],
 })
 export class CustomInputComponent implements OnInit {
+  @Input() name = '';
   @Input() type = '';
   @Input() label = '';
   @Input() required = false;
+  @Input()
+  get value() {
+    return this._value;
+  }
+  set value(val: any) {
+    this._value = val;
+    this.valueChange.emit(this._value);
+  }
+  private _value: any = null;
+
+  @Output() valueChange = new EventEmitter<any>();
 
   constructor() {}
 
